f6e1859eb260fd179d8b95315d8f007bdc702a00
Zhao Cao <cz188b@foxmail.com>
2018-02-01 03:48:51 +0800

    add some drracket configurations

---
+++ drracket-keybindings.rkt
@@ -0,0 +1,12 @@
+#lang s-exp framework/keybinding-lang
+
+(define (command-bind key command)
+  (keybinding
+   key
+   (λ (editor evt)
+     (send (send editor get-keymap) call-function
+           command editor evt #t))))
+
+(command-bind "?:a:semicolon" "uncomment")
+(command-bind "d:semicolon" "comment-out")
+(command-bind "c:c;c:z" "shift-focus")
